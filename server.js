512c6a91af7125cef470dcc450b6402bb0ff43ef
Mariana Murillo <mariana@primarix.com>
2022-06-25 17:44:52 -0600

    Add register endpoint

--- server.js
+++ server.js
@@ -4,8 +4,33 @@ const server = express();
 
 dotenv.config();
 server.listen(process.env.PORT || 4545);
-console.log(`The server is listening on port http://0.0.0.0:${process.env.PORT || 4545}`);
+console.log(`The server is listening on port http://localhost:${process.env.PORT || 4545}`);
 
-server.get('/', function (req, res) {
+server.use(express.json());
+
+server.get('/', (req, res) => {
     res.send("Welcome.");
+});
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+server.post('/users', async (req, res) => {
+    const userPayload = req.body;
+    try {
+        const testUser = {
+            "id":1,
+            "name": userPayload.name,
+            "lastname": userPayload.lastname,
+            "email": userPayload.email,
+            "password": userPayload.password//await bcrypt.hash(userPayload.password, saltRounds),
+        };
+        res.json(testUser);
+    } catch (error) {
+        res.status(500).json({
+            message:
+              "Ocurrió un error al crear el usuario. Intente nuevamente. Si el error persiste, contacte al administrador del sistema.",
+            error,
+        });
+    }
 });
