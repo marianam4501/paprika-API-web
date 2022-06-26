e97b040bf294bc9e2b6a6a157aa8bfe50a134b52
Mariana Murillo <mariana@primarix.com>
2022-06-25 18:45:34 -0600

    Add login endpoint

--- server.js
+++ server.js
@@ -23,13 +23,41 @@ server.post('/users', async (req, res) => {
             "name": userPayload.name,
             "lastname": userPayload.lastname,
             "email": userPayload.email,
-            "password": userPayload.password//await bcrypt.hash(userPayload.password, saltRounds),
+            "password": await bcrypt.hash(userPayload.password, saltRounds),
         };
         res.json(testUser);
     } catch (error) {
         res.status(500).json({
             message:
               "Ocurrió un error al crear el usuario. Intente nuevamente. Si el error persiste, contacte al administrador del sistema.",
+            error,
+        });
+    }
+});
+
+server.post('/users/login', async (req, res) => {
+    const userPayload = req.body;
+    const testUser = {
+        "id":1,
+        "name": "test",
+        "lastname": "",
+        "email": "mariana.murilloquintana@ucr.ac.cr",
+        "password": "$2b$10$bu8RfIcSCUYBcjDuyNePYevVcvpHTIK8tlyogugjWGU/tC8VLNtWe" //t3stp4ssw0rd
+    };
+    try {
+        if (userPayload.email != testUser.email || !(await bcrypt.compare(userPayload.password, testUser.password))) {
+            res.status(401).send("Invalid credentials");
+            return;
+        } else {
+            delete testUser.password;
+            res.status(200);
+            res.json(testUser);
+        }
+
+    } catch (error) {
+        res.status(500).json({
+            message:
+              "Ocurrió un error al iniciar sesión. Intente nuevamente. Si el error persiste, contacte al administrador del sistema.",
             error,
         });
     }
