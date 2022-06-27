00e2114124340ed30d4c9a2e588bc95be4728471
Mariana Murillo <mariana@primarix.com>
2022-06-26 20:23:48 -0600

    add list recipes endpoint

--- server.js
+++ server.js
@@ -17,15 +17,22 @@ const saltRounds = 10;
 
 server.post('/users', async (req, res) => {
     const userPayload = req.body;
+    const user = {
+        "id":1,
+        "name": userPayload.name,
+        "lastname": userPayload.lastname,
+        "email": userPayload.email,
+        "password": await bcrypt.hash(userPayload.password, saltRounds)
+    };
     try {
-        const testUser = {
-            "id":1,
-            "name": userPayload.name,
-            "lastname": userPayload.lastname,
-            "email": userPayload.email,
-            "password": await bcrypt.hash(userPayload.password, saltRounds),
-        };
-        res.json(testUser);
+        const existingUser = testUsers.find(u => u.email === user.email);
+        if(!existingUser){
+            res.status(200);
+            res.json(user);
+        } else {
+            res.status(400).send("Ya hay un usuario registrado con ese correo electrónico.");
+            return;
+        }
     } catch (error) {
         res.status(500).json({
             message:
@@ -35,25 +42,41 @@ server.post('/users', async (req, res) => {
     }
 });
 
-server.post('/users/login', async (req, res) => {
-    const userPayload = req.body;
-    const testUser = {
+const testUsers = [
+    {
         "id":1,
-        "name": "test",
-        "lastname": "",
+        "name": "Test",
+        "lastname": "User",
+        "email": "test@ucr.ac.cr",
+        "password": "$2b$10$bu8RfIcSCUYBcjDuyNePYevVcvpHTIK8tlyogugjWGU/tC8VLNtWe" //t3stp4ssw0rd
+    },
+    {
+        "id":2,
+        "name": "Nathan",
+        "lastname": "Miranda",
+        "email": "nathan.miranda@ucr.ac.cr",
+        "password": "$2b$10$bu8RfIcSCUYBcjDuyNePYevVcvpHTIK8tlyogugjWGU/tC8VLNtWe" //t3stp4ssw0rd
+    },
+    {
+        "id":3,
+        "name": "Mariana",
+        "lastname": "Murillo",
         "email": "mariana.murilloquintana@ucr.ac.cr",
         "password": "$2b$10$bu8RfIcSCUYBcjDuyNePYevVcvpHTIK8tlyogugjWGU/tC8VLNtWe" //t3stp4ssw0rd
-    };
+    },
+];
+server.post('/users/login', async (req, res) => {
+    const userPayload = req.body;
     try {
-        if (userPayload.email != testUser.email || !(await bcrypt.compare(userPayload.password, testUser.password))) {
+        const user = testUsers.find(u => u.email === userPayload.email);
+        if (!user || !(await bcrypt.compare(userPayload.password, user.password))) {
             res.status(401).send("Invalid credentials");
             return;
         } else {
-            delete testUser.password;
+            delete user.password;
             res.status(200);
-            res.json(testUser);
+            res.json(user);
         }
-
     } catch (error) {
         res.status(500).json({
             message:
@@ -62,3 +85,96 @@ server.post('/users/login', async (req, res) => {
         });
     }
 });
+
+const testRecipes = [
+    {
+        "id":1,
+        "userId": 2,
+        "name": "Pie de limón",
+        "ingredients": [
+            {
+                "id": 1,
+                "name": "Galleta María",
+                "quantity": 3
+            },
+            {
+                "id": 2,
+                "name": "Mantequilla",
+                "quantity": 2
+            },
+            {
+                "id": 3,
+                "name": "Leche condensada",
+                "quantity": 1
+            },
+            {
+                "id": 4,
+                "name": "Limón",
+                "quantity": 3
+            }
+        ],
+        "steps": "Duración: 1 hora. Dificultad: Intermedia.",
+        "image": "https://ibb.co/X5dPNp5"
+    },
+    {
+        "id":2,
+        "userId": 2,
+        "name": "Queque de banano",
+        "ingredients": [
+            {
+                "id": 1,
+                "name": "Banano",
+                "quantity": 4
+            },
+            {
+                "id": 2,
+                "name": "Harina",
+                "quantity": 3
+            },
+            {
+                "id": 3,
+                "name": "Leche",
+                "quantity": 2
+            }
+        ],
+        "steps": "Duración: 1 hora. Dificultad: Fácil.",
+        "image": "https://ibb.co/JzS1rm8"
+    },
+    {
+        "id":3,
+        "userId": 3,
+        "name": "Spaghetti a la boloñesa",
+        "ingredients": [
+            {
+                "id": 1,
+                "name": "Spaghetti",
+                "quantity": 1
+            },
+            {
+                "id": 2,
+                "name": "Tomates",
+                "quantity": 3
+            },
+            {
+                "id": 3,
+                "name": "Carne molida",
+                "quantity": 1
+            }
+        ],
+        "steps": "Duración: 45 minutos. Dificultad: Fácil.",
+        "image": "https://ibb.co/51zJBVg"
+    },
+];
+server.get('/recipes', async (req, res) => {
+    try {
+        res.status(200);
+        res.json(testRecipes);
+    } catch (error) {
+        res.status(500).json({
+            message:
+              "Ocurrió un error al iniciar sesión. Intente nuevamente. Si el error persiste, contacte al administrador del sistema.",
+            error,
+        });
+    }
+});
+
