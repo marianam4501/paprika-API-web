0e7d22f7491995dbc19deb3e626ce8083a7e807a
Mariana Murillo <mariana@primarix.com>
2022-07-01 23:06:04 -0600

    add recipes default data

---
+++ data/recipes.js
@@ -0,0 +1,89 @@
+exports.testRecipes = [
+    {
+        "id":1,
+        "userId": 2,
+        "name": "Pie de limón",
+        "ingredients": [
+            {
+                "id": 1,
+                "name": "Galleta María",
+                "measurement": "unidad(es)",
+                "quantity": 3
+            },
+            {
+                "id": 2,
+                "name": "Mantequilla",
+                "measurement": "unidad(es)",
+                "quantity": 2
+            },
+            {
+                "id": 3,
+                "name": "Leche condensada",
+                "measurement": "taza(s)",
+                "quantity": 1
+            },
+            {
+                "id": 4,
+                "name": "Limón",
+                "measurement": "mililitro(s)",
+                "quantity": 250
+            }
+        ],
+        "steps": "Duración: 1 hora. Dificultad: Intermedia.",
+        "image": "https://ibb.co/X5dPNp5"
+    },
+    {
+        "id":2,
+        "userId": 2,
+        "name": "Queque de banano",
+        "ingredients": [
+            {
+                "id": 1,
+                "name": "Banano",
+                "measurement": "unidad(es)",
+                "quantity": 4
+            },
+            {
+                "id": 2,
+                "name": "Harina",
+                "measurement": "taza(s)",
+                "quantity": 3
+            },
+            {
+                "id": 3,
+                "name": "Leche",
+                "measurement": "taza(s)",
+                "quantity": 2
+            }
+        ],
+        "steps": "Duración: 1 hora. Dificultad: Fácil.",
+        "image": "https://ibb.co/JzS1rm8"
+    },
+    {
+        "id":3,
+        "userId": 3,
+        "name": "Spaghetti a la boloñesa",
+        "ingredients": [
+            {
+                "id": 1,
+                "name": "Spaghetti",
+                "measurement": "unidad(es)",
+                "quantity": 1
+            },
+            {
+                "id": 2,
+                "name": "Tomates",
+                "measurement": "unidad(es)",
+                "quantity": 3
+            },
+            {
+                "id": 3,
+                "name": "Carne molida",
+                "measurement": "kilogramo(s)",
+                "quantity": 1
+            }
+        ],
+        "steps": "Duración: 45 minutos. Dificultad: Fácil.",
+        "image": "https://ibb.co/51zJBVg"
+    },
+];
