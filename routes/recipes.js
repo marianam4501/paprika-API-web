aa31764f2f9246fd47572c791b5b4627a4323e52
Mariana Murillo <mariana@primarix.com>
2022-07-03 15:21:46 -0600

    adapt routes so that they use validators

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { createRecipe, getFeed, getRecipeById } = require('../controllers/recipes');
+const { validateSchema } = require('../middlewares/validation');
+const { createRecipeSchema } = require('../validators/recipes');
 const router = express.Router();
 
-router.route('/').post(createRecipe);
+router.route('/').post(/*[validateSchema(createRecipeSchema)], */createRecipe);
 
 router.route('/feed').get(getFeed);
 
