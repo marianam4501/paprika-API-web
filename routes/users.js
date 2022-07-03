041626f8ae9a84d61dab20c1b7f8e6aae2008050
Mariana Murillo <mariana@primarix.com>
2022-07-03 15:21:27 -0600

    adapt routes so that they use validators

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const { createUser, login, recoverPassword, resetPassword, listUsers, userById } = require('../controllers/users');
 const { checkUserIsAuthenticated, checkRoles } = require('../middlewares/authentication');
+const { validateSchema } = require('../middlewares/validation');
 const router = express.Router();
 const { ROLES } = require("../utils/constants");
+const { createUserSchema, loginSchema, recoverPasswordSchema, resetPasswordSchema } = require('../validators/users');
 
 router.route('/')
-    .post(createUser)
+    .post([validateSchema(createUserSchema)], createUser)
     .get([checkUserIsAuthenticated, checkRoles([ROLES.ADMIN])],listUsers);
 
-router.route('/login').post(login);
+router.route('/login').post([validateSchema(loginSchema)], login);
 
-router.route('/recover-password').post(recoverPassword);
+router.route('/recover-password').post([validateSchema(recoverPasswordSchema)],recoverPassword);
 
-router.route('/reset-password').patch(resetPassword);
+router.route('/reset-password').patch([validateSchema(resetPasswordSchema)],resetPassword);
 
 
 router.route('/:id').get(userById);
