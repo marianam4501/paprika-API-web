96f1ba68883fdaf13f24077175013b8cd05d1820
Mariana Murillo <mariana@primarix.com>
2022-07-09 17:56:11 -0600

    add photo to Nathan account

--- data/users.js
+++ data/users.js
@@ -13,7 +13,7 @@ exports.testUsers = [
         "lastname": "Miranda",
         "email": "nathan.miranda@ucr.ac.cr",
         "password": "$2b$10$bu8RfIcSCUYBcjDuyNePYevVcvpHTIK8tlyogugjWGU/tC8VLNtWe", //t3stp4ssw0rd
-        "profile_picture": ""
+        "profile_picture": "https://ci0137.s3.amazonaws.com/paprika/nathan.jpg"
     },
     {
         "id":3,
